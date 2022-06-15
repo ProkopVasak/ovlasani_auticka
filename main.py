51c7edcbd45b6835270c5e86c7b7b6971bc9a56a
ProkopVasak <91657857+ProkopVasak@users.noreply.github.com>
2022-06-15 23:17:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-speedFactor = 80
-l = DigitalPin.P13
-r = DigitalPin.P14
+speedFactor = -80
 pin_Trig = DigitalPin.P8
 pin_Echo = DigitalPin.P15
-whiteline = 1
+l = DigitalPin.P13
+r = DigitalPin.P14
+whiteline = 0
 connected = 0
-manual = 0
-switch = 0
+manual = False
+
 strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
 pins.set_pull(l, PinPullMode.PULL_NONE)
 pins.set_pull(r, PinPullMode.PULL_NONE)
@@ -15,12 +15,11 @@ basic.show_string("S")
 
 def motor_run(left = 0, right = 0, speed_factor = 80):
     PCAmotor.motor_run(PCAmotor.Motors.M1, Math.map(Math.constrain(left * (speedFactor / 100), -100, 100), -100, 100, -255, 255))
-    PCAmotor.motor_run(PCAmotor.Motors.M2, Math.map(Math.constrain(-1 * right * (speedFactor / 100), -100, 100), -100, 100, -255, 255))
+    PCAmotor.motor_run(PCAmotor.Motors.M2, Math.map(Math.constrain(right * (speedFactor / 100), -100, 100), -100, 100, -255, 255))
 
 def motor_stop():
     PCAmotor.motor_stop(PCAmotor.Motors.M1)
     PCAmotor.motor_stop(PCAmotor.Motors.M2)
-
 def on_bluetooth_connected():
     global connected
     basic.show_icon(IconNames.HEART)
@@ -29,17 +28,31 @@ def on_bluetooth_connected():
         uartData = bluetooth.uart_read_until(serial.delimiters(Delimiters.HASH))
         console.log_value("data", uartData)
         if uartData == "A":
-            motor_run(-70, 80, -80)
+            motor_run(70, 70)
             basic.pause(500)
             motor_stop()
+        elif uartData == "B":
+            motor_run(-70, -70)
+            basic.pause(300)
+            motor_stop()    
         elif uartData == "C":
-            motor_run(-70, 0, -80)
+            motor_run(70, 0)
             basic.pause(300)
             motor_stop()
         elif uartData == "D":
-            motor_run(0, 80, -80)
+            motor_run(0, 70)
             basic.pause(300)
             motor_stop()
+        elif uartData == "G":
+            motor_run(80, -80)   
+            basic.pause(580) 
+            motor_stop()
+        elif uartData == "F":
+            motor_run(-80, 80)
+            basic.pause(580)
+            motor_stop()
+        elif uartData == "E":
+            motor_stop()    
 bluetooth.on_bluetooth_connected(on_bluetooth_connected)
 
 def on_bluetooth_disconnected():
@@ -50,31 +63,30 @@ bluetooth.on_bluetooth_disconnected(on_bluetooth_disconnected)
 
 def manual_movement():
     global manual
-    if manual == 0:
-        maual = 1
-    else:
-        manual = 0
-input.on_button_pressed(Button.AB, manual_movement)
+    if manual == False: manual = True
+    else: manual = False
+    print(manual)    
+input.on_button_pressed(Button.A, manual_movement)
 
 
 
 
 def movement():
     if connected ==0:
-        def on_forever():    
-            global l, r
-            obstacle_distance = sonar.ping(pin_Trig, pin_Echo, PingUnit.CENTIMETERS, 100)
-            l = pins.digital_read_pin(DigitalPin.P13)
-            r = pins.digital_read_pin(DigitalPin.P14)
-            if l == 0 and r == 1:
-                motor_run(0, 80, -80)
-            elif l == 1 and r == 0:
-                motor_run(-70, 0, -80)
-            else:
-                motor_run(-70, 80, -80)
-            basic.pause(50) #reakční frekvence 20 Hz
-        basic.forever(on_forever)    
-        
-            
-            
+        def on_forever():
+            global manual, connected
+            if manual == True:
+                l = pins.digital_read_pin(DigitalPin.P13)
+                r = pins.digital_read_pin(DigitalPin.P14)
+                if l == whiteline and r != whiteline:
+                    motor_run(0, 50)
+                elif l != whiteline and r == whiteline:
+                    motor_run(50, 0)
+                elif l == whiteline and r == whiteline:
+                    motor_run(50, 50)
+                elif l != whiteline and r != whiteline:
+                    motor_run(50, 50)
+                basic.pause(50) #reakční frekvence 20 Hz
+            # print(l+""+r)
+        basic.forever(on_forever)
 basic.forever(movement)
